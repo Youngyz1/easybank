260f2799321dee85c11ab1cb48bf9f3022e29a9f
Youngyz1 <uche2net@gmail.com>
2026-02-24 23:45:44 +0100

    updated waf.tf

--- easybank-infra/waf.tf
+++ easybank-infra/waf.tf
@@ -5,27 +5,67 @@ resource "aws_wafv2_web_acl" "easybank" {
   name        = "easybank-waf"
   description = "WAF for EasyBank ALB"
   scope       = "REGIONAL"
-
   default_action {
     allow {}
+  }
+
+  # Rule 0: Allow file uploads on register pages
+  rule {
+    name     = "AllowRegisterFileUploads"
+    priority = 0
+    action {
+      allow {}
+    }
+    statement {
+      or_statement {
+        statement {
+          byte_match_statement {
+            search_string         = "/page-register3.php"
+            positional_constraint = "CONTAINS"
+            field_to_match {
+              uri_path {}
+            }
+            text_transformation {
+              priority = 0
+              type     = "NONE"
+            }
+          }
+        }
+        statement {
+          byte_match_statement {
+            search_string         = "/page-register4.php"
+            positional_constraint = "CONTAINS"
+            field_to_match {
+              uri_path {}
+            }
+            text_transformation {
+              priority = 0
+              type     = "NONE"
+            }
+          }
+        }
+      }
+    }
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "AllowRegisterFileUploads"
+      sampled_requests_enabled   = true
+    }
   }
 
   # Rule 1: Block common SQL injection
   rule {
     name     = "AWSManagedRulesSQLiRuleSet"
     priority = 1
-
     override_action {
       none {}
     }
-
     statement {
       managed_rule_group_statement {
         name        = "AWSManagedRulesSQLiRuleSet"
         vendor_name = "AWS"
       }
     }
-
     visibility_config {
       cloudwatch_metrics_enabled = true
       metric_name                = "SQLiRuleSet"
@@ -37,18 +77,27 @@ resource "aws_wafv2_web_acl" "easybank" {
   rule {
     name     = "AWSManagedRulesCommonRuleSet"
     priority = 2
-
     override_action {
       none {}
     }
-
     statement {
       managed_rule_group_statement {
         name        = "AWSManagedRulesCommonRuleSet"
         vendor_name = "AWS"
+        rule_action_override {
+          name = "SizeRestrictions_BODY"
+          action_to_use {
+            count {}
+          }
+        }
+        rule_action_override {
+          name = "SizeRestrictions_Querystring"
+          action_to_use {
+            count {}
+          }
+        }
       }
     }
-
     visibility_config {
       cloudwatch_metrics_enabled = true
       metric_name                = "CommonRuleSet"
@@ -60,18 +109,15 @@ resource "aws_wafv2_web_acl" "easybank" {
   rule {
     name     = "AWSManagedRulesKnownBadInputsRuleSet"
     priority = 3
-
     override_action {
       none {}
     }
-
     statement {
       managed_rule_group_statement {
         name        = "AWSManagedRulesKnownBadInputsRuleSet"
         vendor_name = "AWS"
       }
     }
-
     visibility_config {
       cloudwatch_metrics_enabled = true
       metric_name                = "KnownBadInputsRuleSet"
